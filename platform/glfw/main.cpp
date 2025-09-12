7c65c6ff63e67e2a888b4e9fed9289ae32b4947b
Birk Skyum <birk.skyum@pm.me>
2025-09-13 01:03:57 +0200

    remove all logging

--- platform/glfw/main.cpp
+++ platform/glfw/main.cpp
@@ -28,7 +28,6 @@ GLFWView* view = nullptr;
 
 void quit_handler(int) {
     if (view) {
-        mbgl::Log::Info(mbgl::Event::Setup, "waiting for quit...");
         view->setShouldClose();
     } else {
         exit(0);
@@ -96,7 +95,6 @@ int main(int argc, char* argv[]) {
 #endif
 
     if (benchmark) {
-        mbgl::Log::Info(mbgl::Event::General, "BENCHMARK MODE: Some optimizations are disabled.");
     }
 
     // Set access token if present
@@ -117,11 +115,8 @@ int main(int argc, char* argv[]) {
     if (!settings.online) {
         if (onlineFileSource) {
             onlineFileSource->setProperty("online-status", false);
-            mbgl::Log::Warning(mbgl::Event::Setup, "Application is offline. Press `O` to toggle online status.");
         } else {
-            mbgl::Log::Warning(mbgl::Event::Setup,
-                               "Network resource provider is not available, only local "
-                               "requests are supported.");
+
         }
     }
 
@@ -151,16 +146,12 @@ int main(int argc, char* argv[]) {
 
     view->setOnlineStatusCallback([&settings, onlineFileSource]() {
         if (!onlineFileSource) {
-            mbgl::Log::Warning(mbgl::Event::Setup,
-                               "Cannot change online status. Network resource provider is not "
-                               "available.");
+
             return;
         }
         settings.online = !settings.online;
         onlineFileSource->setProperty("online-status", settings.online);
-        mbgl::Log::Info(mbgl::Event::Setup,
-                        std::string("Application is ") + (settings.online ? "online" : "offline") +
-                            ". Press `O` to toggle online status.");
+
     });
 
     view->setChangeStyleCallback([&map, &orderedStyles]() {
@@ -174,7 +165,6 @@ int main(int argc, char* argv[]) {
         map.getStyle().loadURL(newStyle.getUrl());
         view->setWindowTitle(newStyle.getName());
 
-        mbgl::Log::Info(mbgl::Event::Setup, "Changed style to: " + newStyle.getName());
     });
 
     // Resource loader controls top-level request processing and can resume /
@@ -199,7 +189,6 @@ int main(int argc, char* argv[]) {
     view->setResetCacheCallback([databaseFileSource]() {
         databaseFileSource->resetDatabase([](const std::exception_ptr& ex) {
             if (ex) {
-                mbgl::Log::Error(mbgl::Event::Database, "Failed to reset cache: " + mbgl::util::toString(ex));
             }
         });
     });
@@ -230,10 +219,6 @@ int main(int argc, char* argv[]) {
     settings.pitch = *camera.pitch;
     settings.debug = mbgl::EnumType(map.getDebug());
     settings.save();
-    mbgl::Log::Info(mbgl::Event::General,
-                    "Exit location: --lat=\"" + std::to_string(settings.latitude) + "\" --lon=\"" +
-                        std::to_string(settings.longitude) + "\" --zoom=\"" + std::to_string(settings.zoom) +
-                        "\" --bearing=\"" + std::to_string(settings.bearing) + "\"");
 
     view = nullptr;
 
